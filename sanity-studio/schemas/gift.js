a2e472add2aa8535ecfea704d2733681169da354
vicbergquist <victoria.bergquist@gmail.com>
2021-02-06 21:36:05 +0100

    add price range to gift schema

--- sanity-studio/schemas/gift.js
+++ sanity-studio/schemas/gift.js
@@ -1,42 +1,55 @@
 export default {
-	title: "Gift",
-	name: "gift",
-	type: "document",
-	fields: [
-		{
-			title: "Name",
-			name: "name",
-			type: "string",
-		},
-		{
-			title: "Manufacturer",
-			name: "manufacturer",
-			type: "string",
-		},
-		{
-			title: "Link",
-			name: "link",
-			type: "string",
-		},
-		{
-			title: "Image",
-			name: "image",
-			type: "image",
-			fields: [
-				{
-					name: "caption",
-					title: "Caption",
-					type: "string",
-				},
-			],
-		},
-		{
-			title: "Purchased",
-			name: "isPurchased",
-			type: "boolean",
-		},
-	],
-	initialValue: {
-		isPurchased: false,
-	},
-};
+  title: "Gift",
+  name: "gift",
+  type: "document",
+  liveEdit: true,
+  fields: [
+    {
+      title: "Name",
+      name: "name",
+      type: "string",
+    },
+    {
+      title: "Manufacturer",
+      name: "manufacturer",
+      type: "string",
+    },
+    {
+      title: "Price category",
+      type: "number",
+      name: "priceCategory",
+      options: {
+        list: [
+          { title: "$", value: 1 },
+          { title: "$$", value: 2 },
+          { title: "$$$", value: 3 },
+        ],
+      },
+    },
+    {
+      title: "Link",
+      name: "link",
+      type: "string",
+    },
+    {
+      title: "Image",
+      name: "image",
+      type: "image",
+      fields: [
+        {
+          name: "caption",
+          title: "Caption",
+          type: "string",
+        },
+      ],
+    },
+    {
+      title: "Purchased",
+      name: "isPurchased",
+      type: "boolean",
+    },
+  ],
+  initialValue: {
+    isPurchased: false,
+  },
+}
