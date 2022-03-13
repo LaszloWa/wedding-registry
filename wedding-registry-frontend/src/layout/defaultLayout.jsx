38921c53e1187259ea070c98a3d3725507cd970d
Laszlo <44098779+LaszloWa@users.noreply.github.com>
2022-03-13 13:29:42 +0100

    Formatting

--- wedding-registry-frontend/src/layout/defaultLayout.jsx
+++ wedding-registry-frontend/src/layout/defaultLayout.jsx
@@ -1,20 +1,20 @@
-import React from "react"
+import React from "react";
 import {
-  Stack,
-  Flex,
-  Text,
-  ThemeProvider,
-  Card,
-  Container,
-  ToastProvider,
-  Button,
-} from "@sanity/ui"
-import { weddingTheme } from "../theme"
-import { Navigation } from "./navigation"
-import { BrowserRouter as Router, Switch } from "react-router-dom"
-import styled, { createGlobalStyle } from "styled-components"
-import { LeaveIcon } from "@sanity/icons"
-import { useAuth } from "../providers/auth-provider"
+	Stack,
+	Flex,
+	Text,
+	ThemeProvider,
+	Card,
+	Container,
+	ToastProvider,
+	Button,
+} from "@sanity/ui";
+import { weddingTheme } from "../theme";
+import { Navigation } from "./navigation";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import styled, { createGlobalStyle } from "styled-components";
+import { LeaveIcon } from "@sanity/icons";
+import { useAuth } from "../providers/auth-provider";
 
 const GlobalStyle = createGlobalStyle`
 
@@ -28,64 +28,64 @@ const GlobalStyle = createGlobalStyle`
 *::after {
     box-sizing: inherit;
 }
-`
+`;
 
 const BackgroundImage = styled.img`
-  position: absolute;
-  bottom: -20px;
-  width: calc(100% - 2px);
-  object-fit: cover;
-  min-height: 200px;
-`
+	position: absolute;
+	bottom: -20px;
+	width: calc(100% - 2px);
+	object-fit: cover;
+	min-height: 200px;
+`;
 
 export const DefaultLayout = ({ user, children }) => {
-  const { logout } = useAuth()
+	const { logout } = useAuth();
 
-  return (
-    <>
-      <GlobalStyle />
-      <BackgroundImage src="/background.png" />
-      <ThemeProvider theme={weddingTheme}>
-        <ToastProvider>
-          <Router>
-            <Switch>
-              <Card
-                paddingBottom={4}
-                style={{ minHeight: "100vh", width: "100%" }}
-                height="fill"
-              >
-                <Container height="fill">
-                  <Stack space={4} padding={2}>
-                    {user && (
-                      <Flex justify="center">
-                        <Navigation user={user} />
-                      </Flex>
-                    )}
-                    <Flex justify="center" height="fill">
-                      {children}
-                    </Flex>
-                    <Stack space={3}>
-                      {user && (
-                        <Flex justify="center">
-                          <Button
-                            mode="bleed"
-                            text="Log out"
-                            icon={LeaveIcon}
-                            onClick={logout}
-                          />
-                        </Flex>
-                      )}
-                      <Text muted size={2} style={{ textAlign: "center" }}>
-                        Having issues? Please reach out to us :)
-                      </Text>
-                    </Stack>
-                  </Stack>
-                </Container>
-              </Card>
-            </Switch>
-          </Router>
-        </ToastProvider>
-      </ThemeProvider>
-    </>
-  )
-}
+	return (
+		<>
+			<GlobalStyle />
+			<BackgroundImage src="/background.png" />
+			<ThemeProvider theme={weddingTheme}>
+				<ToastProvider>
+					<Router>
+						<Switch>
+							<Card
+								paddingBottom={4}
+								style={{ minHeight: "100vh", width: "100%" }}
+								height="fill"
+							>
+								<Container height="fill">
+									<Stack space={4} padding={2}>
+										{user && (
+											<Flex justify="center">
+												<Navigation user={user} />
+											</Flex>
+										)}
+										<Flex justify="center" height="fill">
+											{children}
+										</Flex>
+										<Stack space={3}>
+											{user && (
+												<Flex justify="center">
+													<Button
+														mode="bleed"
+														text="Log out"
+														icon={LeaveIcon}
+														onClick={logout}
+													/>
+												</Flex>
+											)}
+											<Text muted size={2} style={{ textAlign: "center" }}>
+												Having issues? Please reach out to us :)
+											</Text>
+										</Stack>
+									</Stack>
+								</Container>
+							</Card>
+						</Switch>
+					</Router>
+				</ToastProvider>
+			</ThemeProvider>
+		</>
+	);
+};
