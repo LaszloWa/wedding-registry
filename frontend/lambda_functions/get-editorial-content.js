f1b829a0cf30ae475a0b8117dc0190995ac2e0bc
Laszlo <44098779+LaszloWa@users.noreply.github.com>
2022-03-19 14:39:33 +0100

    Adds version to sanityClient

--- frontend/lambda_functions/get-editorial-content.js
+++ frontend/lambda_functions/get-editorial-content.js
@@ -5,6 +5,7 @@ import imageUrlBuilder from "@sanity/image-url";
 const client = sanityClient({
 	projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
 	dataset: "production",
+	apiVersion: "2022-03-18",
 	token: process.env.REACT_APP_SANITY_TOKEN,
 	useCdn: false,
 });
--- frontend/lambda_functions/login.js
+++ frontend/lambda_functions/login.js
@@ -6,6 +6,7 @@ import { serialize } from "cookie";
 const client = sanityClient({
 	projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
 	dataset: "production",
+	apiVersion: "2022-03-18",
 	token: process.env.REACT_APP_SANITY_TOKEN, // or leave blank to be anonymous user
 	useCdn: false, // `false` if you want to ensure fresh data
 });
