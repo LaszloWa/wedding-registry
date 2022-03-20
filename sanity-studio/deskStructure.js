f75386c378607a901c83a4be0bfe87d78f802fb5
Victoria Bergquist <victoria.bergquist@gmail.com>
2022-03-20 14:51:51 +0100

    add all types to desk structure

--- sanity-studio/deskStructure.js
+++ sanity-studio/deskStructure.js
@@ -1,67 +1,73 @@
-import React from "react";
-import S from "@sanity/desk-tool/structure-builder";
-import { /* PersonPreview, GiftPreview, */ Icon } from "./components";
+import React from "react"
+import S from "@sanity/desk-tool/structure-builder"
+import { /* PersonPreview, GiftPreview, */ Icon } from "./components"
 
 export const getDefaultDocumentNode = ({ schemaType }) => {
-	// if (schemaType === "gift") {
-	//   return S.document().views([
-	//     S.view.form(),
-	//     S.view.component(GiftPreview).title("Preview"),
-	//   ])
-	// }
+  // if (schemaType === "gift") {
+  //   return S.document().views([
+  //     S.view.form(),
+  //     S.view.component(GiftPreview).title("Preview"),
+  //   ])
+  // }
 
-	// if (schemaType === "person") {
-	//   return S.document().views([
-	//     S.view.component(PersonPreview).title("Summary"),
-	//     S.view.form(),
-	//   ])
-	// }
+  // if (schemaType === "person") {
+  //   return S.document().views([
+  //     S.view.component(PersonPreview).title("Summary"),
+  //     S.view.form(),
+  //   ])
+  // }
 
-	return S.document().views([S.view.form()]);
-};
+  return S.document().views([S.view.form()])
+}
 
 export default () =>
-	S.list()
-		.title("Content")
-		.items([
-			S.listItem()
-				.title("Website")
-				.icon(() => <Icon emoji="🌿" />)
-				.child(S.documentTypeList("page").title("Website")),
-			S.listItem()
-				.title("Gifts")
-				.icon(() => <Icon emoji="🎁" />)
-				.child(
-					S.list()
-						.title("Gifts")
-						.items([
-							S.listItem()
-								.title("Available")
-								.icon(() => <Icon emoji="✨" />)
-								.child((giftId) =>
-									S.documentList()
-										.title("Available gifts")
-										.filter("!isReserved"),
-								),
-							S.listItem()
-								.title("Reserved")
-								.icon(() => <Icon emoji="✔️" />)
-								.child((giftId) =>
-									S.documentList().title("Reserved gifts").filter("isReserved"),
-								),
-							,
-							S.listItem()
-								.title("All gifts")
-								.icon(() => <Icon emoji="🎁" />)
-								.child(S.documentTypeList("gift").title("All gifts")),
-						]),
-				),
-			S.listItem()
-				.title("Persons")
-				.icon(() => <Icon emoji="🐱" />)
-				.child(S.documentTypeList("person").title("Persons")),
-			S.listItem()
-				.title("RSVP")
-				.icon(() => <Icon emoji="📬" />)
-				.child(S.documentTypeList("rsvp").title("RSVP")),
-		]);
+  S.list()
+    .title("Content")
+    .items([
+      S.listItem()
+        .title("Website")
+        .icon(() => <Icon emoji="🌿" />)
+        .child(S.documentTypeList("page").title("Website")),
+      S.listItem()
+        .title("Gifts")
+        .icon(() => <Icon emoji="🎁" />)
+        .child(
+          S.list()
+            .title("Gifts")
+            .items([
+              S.listItem()
+                .title("Available")
+                .icon(() => <Icon emoji="✨" />)
+                .child((giftId) =>
+                  S.documentList()
+                    .title("Available gifts")
+                    .filter("!isReserved")
+                ),
+              S.listItem()
+                .title("Reserved")
+                .icon(() => <Icon emoji="✔️" />)
+                .child((giftId) =>
+                  S.documentList().title("Reserved gifts").filter("isReserved")
+                ),
+              ,
+              S.listItem()
+                .title("All gifts")
+                .icon(() => <Icon emoji="🎁" />)
+                .child(S.documentTypeList("gift").title("All gifts")),
+            ])
+        ),
+      S.listItem()
+        .title("Persons")
+        .icon(() => <Icon emoji="🐱" />)
+        .child(S.documentTypeList("person").title("Persons")),
+      S.listItem()
+        .title("RSVPs")
+        .icon(() => <Icon emoji="📬" />)
+        .child(S.documentTypeList("rsvp").title("RSVPs")),
+      ...S.documentTypeListItems().filter(
+        (listItem) =>
+          !["website", "page", "person", "gift", "rsvp"].includes(
+            listItem.getId()
+          )
+      ),
+    ])
