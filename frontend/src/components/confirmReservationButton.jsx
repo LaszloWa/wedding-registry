d49c4a3ae7aa54848938a9662b9b895cf41b3931
Victoria Bergquist <victoria.bergquist@gmail.com>
2022-03-22 13:27:49 +0100

    update registry

--- frontend/src/components/confirmReservationButton.jsx
+++ frontend/src/components/confirmReservationButton.jsx
@@ -7,6 +7,7 @@ import {
   Tooltip,
   Box,
   Inline,
+  Flex,
 } from "@sanity/ui"
 import React, { useState } from "react"
 import { HeartIcon } from "@sanity/icons"
@@ -15,11 +16,16 @@ import { Prompt } from "."
 
 const ReserveButton = styled.button`
   pointer-events: auto;
-  padding: 2px 1em;
+  padding: 5px 1em;
   background: none;
   border: none;
   box-shadow: 0;
   margin: auto;
+  border-radius: 5px;
+
+  &:hover {
+    background: #7192691f;
+  }
 
   &:hover [data-sanity-icon] {
     color: #719269;
@@ -41,7 +47,12 @@ const StyledButton = styled(Button)`
   }
 `
 
-export const ConfirmReservationButton = ({ gift, isReserved, onConfirm }) => {
+export const ConfirmReservationButton = ({
+  gift,
+  isReserved,
+  onConfirm,
+  isHoneymoonFund,
+}) => {
   const [isOpen, setIsOpen] = useState(false)
 
   const handleCloseDialog = () => {
@@ -57,27 +68,31 @@ export const ConfirmReservationButton = ({ gift, isReserved, onConfirm }) => {
     handleCloseDialog()
   }
 
+  const headerText = isHoneymoonFund ? (
+    "Donate to our honeymoon fund"
+  ) : (
+    <>
+      Are you sure you want to reserve{" "}
+      <strong>
+        {gift?.manufacturer ? `${gift?.manufacturer} - ` : ""}
+        {gift?.name}
+      </strong>
+      ?
+    </>
+  )
+
   return (
     <>
       {isOpen && (
         <Dialog
           width={1}
-          header={
-            <>
-              Are you sure you want to reserve{" "}
-              <strong>
-                {gift?.manufacturer ? `${gift?.manufacturer} - ` : ""}
-                {gift?.name}
-              </strong>
-              ?
-            </>
-          }
+          header={headerText}
           onClose={handleCloseDialog}
           footer={
             <Box paddingX={4} paddingY={2}>
               <Grid columns={[1, 1, 2]} gap={2}>
                 <Button
-                  text="Cancel"
+                  text={isHoneymoonFund ? "Close" : "Cancel"}
                   mode="bleed"
                   onClick={handleCloseDialog}
                   fontSize={3}
@@ -85,7 +100,7 @@ export const ConfirmReservationButton = ({ gift, isReserved, onConfirm }) => {
                 <StyledButton
                   data-confirm="true"
                   text="Confirm"
-                  onClick={handleConfirm}
+                  onClick={isHoneymoonFund ? handleCloseDialog : handleConfirm}
                   fontSize={3}
                 />
               </Grid>
@@ -94,7 +109,44 @@ export const ConfirmReservationButton = ({ gift, isReserved, onConfirm }) => {
         >
           <Box padding={4}>
             <Stack space={4}>
-              {(gift?.links || []).length > 0 && (
+              {isHoneymoonFund && (
+                <>
+                  <Text size={3}>
+                    Donating to our honeymoon fund can be done using Vipps or
+                    PayPal, depending on your location.
+                  </Text>
+                  <Stack
+                    as="ul"
+                    style={{ listStyleType: "disc" }}
+                    marginLeft={5}
+                    space={3}
+                  >
+                    {gift.links &&
+                      gift.links.map((link) => (
+                        <Text
+                          size={3}
+                          style={{ display: "list-item" }}
+                          key={link.label}
+                        >
+                          {link.country}:{" "}
+                          {link?.href ? (
+                            <a href={link.href}>{link.label}</a>
+                          ) : (
+                            link.label
+                          )}
+                        </Text>
+                      ))}
+                  </Stack>
+                  <Text size={2} muted>
+                    If you need help donating, please reach out to Gabriella on{" "}
+                    <a href="https://www.facebook.com/gabriella.halvorsen">
+                      Facebook
+                    </a>{" "}
+                    or by phone (+47 958 09 261).
+                  </Text>
+                </>
+              )}
+              {!isHoneymoonFund && (gift?.links || []).length > 0 && (
                 <>
                   <Text size={3}>
                     This item is available from the following stores:
@@ -103,36 +155,53 @@ export const ConfirmReservationButton = ({ gift, isReserved, onConfirm }) => {
                     as="ul"
                     style={{ listStyleType: "disc" }}
                     marginLeft={5}
+                    space={3}
                   >
                     {gift.links.map((link) => (
-                      <Text size={3} style={{ display: "list-item" }}>
+                      <Text
+                        size={3}
+                        style={{ display: "list-item" }}
+                        key={link.label}
+                      >
                         <a href={link.href}>{link.label}</a>
                       </Text>
                     ))}
                   </Stack>
                 </>
               )}
-              <Text size={2} muted>
-                If you need help ordering this item, please reach out to
-                Gabriella on{" "}
-                <a href="https://www.facebook.com/gabriella.halvorsen">
-                  Facebook
-                </a>{" "}
-                or by phone (+47 958 09 261). Reserved gifts will be unavailable
-                to others.
-              </Text>
+              {!isHoneymoonFund && (
+                <Text size={2} muted>
+                  If you need help ordering this item, please reach out to
+                  Gabriella on{" "}
+                  <a href="https://www.facebook.com/gabriella.halvorsen">
+                    Facebook
+                  </a>{" "}
+                  or by phone (+47 958 09 261). Reserved gifts will be
+                  unavailable to others.
+                </Text>
+              )}
             </Stack>
           </Box>
         </Dialog>
       )}
-      <ReserveButton onClick={handleOpenDialog} disabled={isReserved}>
-        <Inline space={2}>
-          <Text size={1}>
-            <HeartIcon />
-          </Text>
-          <Text>{isReserved ? "Reserved" : "Reserve"}</Text>
-        </Inline>
-      </ReserveButton>
+      {isReserved ? (
+        <Text muted style={{ margin: "auto" }}>
+          Already reserved
+        </Text>
+      ) : (
+        <ReserveButton onClick={handleOpenDialog}>
+          <Inline space={2}>
+            <Text size={1} muted>
+              <HeartIcon />
+            </Text>
+            {isHoneymoonFund ? (
+              <Text muted>Donate</Text>
+            ) : (
+              <Text muted>Reserve</Text>
+            )}
+          </Inline>
+        </ReserveButton>
+      )}
     </>
   )
 }
