c736f71090665467b184f06793ac2fb4e220b1ff
Laszlo <44098779+LaszloWa@users.noreply.github.com>
2022-07-27 15:41:01 +0200

    Adds console logs

--- frontend/src/api-helper.js
+++ frontend/src/api-helper.js
@@ -1,60 +1,61 @@
 async function sendRequest(endpoint, body, callback) {
-  const requestOptions = {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-    },
-  }
+	const requestOptions = {
+		method: "POST",
+		headers: {
+			Accept: "application/json",
+		},
+	};
 
-  if (body) {
-    requestOptions.headers["Content-Type"] = "application/json"
-    requestOptions.body = JSON.stringify(body)
-  }
+	if (body) {
+		requestOptions.headers["Content-Type"] = "application/json";
+		requestOptions.body = JSON.stringify(body);
+	}
 
-  const response = await fetch(
-    `/.netlify/functions/${endpoint}`,
-    requestOptions
-  )
+	const response = await fetch(
+		`/.netlify/functions/${endpoint}`,
+		requestOptions,
+	);
 
-  if (!response.ok && callback.push) {
-    const statusCode = response.status
-    switch (statusCode) {
-      case 401:
-      case 409:
-        return callback.push({
-          title: "Invalid password",
-          description: "Please double check the password you entered.",
-          status: "error",
-        })
-      default:
-        return callback.push({
-          title: "Oops! Something went wrong!",
-          description: "Please try again or reload the page.",
-          status: "error",
-        })
-    }
-  }
+	if (!response.ok && callback.push) {
+		const statusCode = response.status;
+		switch (statusCode) {
+			case 401:
+			case 409:
+				return callback.push({
+					title: "Invalid password",
+					description: "Please double check the password you entered.",
+					status: "error",
+				});
+			default:
+				console.log("I am firing here");
+				return callback.push({
+					title: "Oops! Something went wrong!",
+					description: "Please try again or reload the page.",
+					status: "error",
+				});
+		}
+	}
 
-  if (response.ok) {
-    switch (endpoint) {
-      case "update-gift":
-        return callback.push({
-          title: "Gift successfully reserved!",
-          status: "success",
-        })
-      case "rsvp":
-        return callback.push({
-          title: "We look forward to seeing you!",
-          status: "success",
-        })
-      case "logout":
-        return
-      case "login":
-        return callback({})
-      case "authenticate":
-        return callback({})
-    }
-  }
+	if (response.ok) {
+		switch (endpoint) {
+			case "update-gift":
+				return callback.push({
+					title: "Gift successfully reserved!",
+					status: "success",
+				});
+			case "rsvp":
+				return callback.push({
+					title: "We look forward to seeing you!",
+					status: "success",
+				});
+			case "logout":
+				return;
+			case "login":
+				return callback({});
+			case "authenticate":
+				return callback({});
+		}
+	}
 }
 
-export default sendRequest
+export default sendRequest;
